d2efaa8d11fb59031fe253598b4f88e2accffd81
Tischler Kristóf <tischler.kristof@gmail.com>
2020-04-26 20:40:06 +0200

    Rename table fields

--- src/Database/migrations/2020_04_25_171543_create_order_product_table.php
+++ src/Database/migrations/2020_04_25_171543_create_order_product_table.php
@@ -15,9 +15,9 @@ class CreateOrderProductTable extends Migration
     {
         Schema::create('order_product', function (Blueprint $table) {
             $table->unsignedBigInteger('order_id')->references('id')->on('orders');
-            $table->unsignedBigInteger('product_id')->references('id')->on('products');
+            $table->unsignedBigInteger('product_catalog_id')->references('id')->on('products');
             $table->unsignedInteger('quantity');
-            $table->primary(['order_id', 'product_id']);
+            $table->primary(['order_id', 'product_catalog_id']);
             $table->timestamps();
         });
     }
