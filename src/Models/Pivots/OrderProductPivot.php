d96a0121aee831ad666d058d01df505850b01e6b
Tischler Kristóf <tischler.kristof@gmail.com>
2020-04-26 23:56:38 +0200

    Field changes

--- src/Models/Pivots/OrderProductPivot.php
+++ src/Models/Pivots/OrderProductPivot.php
@@ -2,12 +2,12 @@
 
 namespace MayIFit\Extension\Shop\Models\Pivots;
 
-use Illuminate\Database\Eloquent\Relations\MorphPivot;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\Model;
 
 use MayIFit\Extension\Shop\Models\Product;
 
-class OrderProductPivot extends MorphPivot
+class OrderProductPivot extends Pivot
 {
     protected $table = 'order_product';
 
@@ -23,7 +23,7 @@ class OrderProductPivot extends MorphPivot
             $parent = $model->pivotParent;
             
             $parent->net_value += $product->net_price;
-            $parent->value += ($product->net_price * (1 + ($product->vat / 100))) * $model->quantity;;
+            $parent->value += ($product->net_price * (1 + ($product->vat / 100))) * $model->quantity;
             $parent->order_quantity = $model->quantity;
             $parent->save();
             return $model;
