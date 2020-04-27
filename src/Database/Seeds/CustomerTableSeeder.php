1099056c5f45258eedd9223b4604438ee0c32330
Tischler Kristóf <tischler.kristof@gmail.com>
2020-04-27 20:19:57 +0200

    Fix classes

--- src/Database/Seeds/CustomerTableSeeder.php
+++ src/Database/Seeds/CustomerTableSeeder.php
@@ -4,11 +4,9 @@ namespace MayIFit\Extension\Shop\Database\Seeds;
 
 use Illuminate\Database\Seeder;
 
-use MayIFit\Extension\Shop\Models\Product;
 use MayIFit\Extension\Shop\Models\Customer;
-use MayIFit\Extension\Shop\Models\Order;
 
-class OrderTableSeeder extends Seeder
+class CustomerTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -17,27 +15,6 @@ class OrderTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Order::class, 100)->make()
-        ->each(function($order) {
-            $randomProducts = Product::all()->random(5)->pluck('catalog_id');
-            $randomCustomer = Customer::all()->random()->pluck('id');
-
-            $syncableData = array();
-
-            foreach ($randomProducts as $product) {
-                $data = array('quantity' => rand(1, 100));
-                array_push($syncableData, $product);
-                array_push($syncableData[$product], $data);
-            }
-
-            $order->products()->sync($syncableData);
-            foreach ($randomProducts as $key => $product) {
-                $order->net_value += $product->net_price * $model->quantity;
-                $order->value += ($product->net_price * (1 + ($product->vat / 100))) * $syncableData[$product]->quantity;
-                $order->order_quantity = $model->quantity;
-            }
-            $order->customer()->attach($randomCustomer);
-            $order->save();
-        });
+        factory(Customer::class, 100)->create();
     }
 }
--- src/Database/Seeds/OrderTableSeeder.php
+++ src/Database/Seeds/OrderTableSeeder.php
@@ -4,9 +4,11 @@ namespace MayIFit\Extension\Shop\Database\Seeds;
 
 use Illuminate\Database\Seeder;
 
+use MayIFit\Extension\Shop\Models\Product;
 use MayIFit\Extension\Shop\Models\Customer;
+use MayIFit\Extension\Shop\Models\Order;
 
-class CustomerTableSeeder extends Seeder
+class OrderTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,6 +17,27 @@ class CustomerTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Customer::class, 100)->create();
+        factory(Order::class, 100)->make()
+        ->each(function($order) {
+            $products = factory(Product::class, 10)->create()->pluck('catalog_id');
+            $randomCustomer = Customer::all()->random()->pluck('id');
+
+            $syncableData = array();
+
+            foreach ($products as $product) {
+                $data = array('quantity' => rand(1, 100));
+                array_push($syncableData, $product);
+                array_push($syncableData[$product], $data);
+            }
+
+            $order->products()->sync($syncableData);
+            foreach ($products as $product) {
+                $order->net_value += $product->net_price * $model->quantity;
+                $order->value += ($product->net_price * (1 + ($product->vat / 100))) * $syncableData[$product]->quantity;
+                $order->order_quantity = $model->quantity;
+            }
+            $order->customer()->attach($randomCustomer);
+            $order->save();
+        });
     }
 }
