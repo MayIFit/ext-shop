f9f88e877d17b8c31afd901dc274f741ffa35801
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-06 22:24:51 +0200

    Fixes

--- src/Database/Seeds/TranslationTableSeeder.php
+++ src/Database/Seeds/TranslationTableSeeder.php
@@ -80,6 +80,95 @@ class TranslationTableSeeder extends Seeder
         ]);
     }
 
+    protected function addOrderRelatedTranslations() {
+        Translation::create([
+            'group' => 'order',
+            'key' => 'order',
+            'text' => ['en' => 'Order', 'hu' => 'Rendelés'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'shipping_address',
+            'text' => ['en' => 'Shipping Address', 'hu' => 'Szállítási Adatok'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'track_order_status',
+            'text' => ['en' => 'Track Order Status', 'hu' => 'Rendelés Követés'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'order_token',
+            'text' => ['en' => 'Order Token', 'hu' => 'Rendelési Azonosító'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'customer',
+            'text' => ['en' => 'Customer', 'hu' => 'Ügyfél'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'title',
+            'text' => ['en' => 'Title', 'hu' => 'Titulus'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'first_name',
+            'text' => ['en' => 'First Name', 'hu' => 'Vezetéknév'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'last_name',
+            'text' => ['en' => 'Last Name', 'hu' => 'Utónév'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'country',
+            'text' => ['en' => 'Country', 'hu' => 'Ország'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'city',
+            'text' => ['en' => 'City', 'hu' => 'Város'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'zip_code',
+            'text' => ['en' => 'Zip Code', 'hu' => 'Irányítószám'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'address',
+            'text' => ['en' => 'Address', 'hu' => 'Közterület neve'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'house_nr',
+            'text' => ['en' => 'House Number', 'hu' => 'Házszám'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'floor',
+            'text' => ['en' => 'Floor', 'hu' => 'Emelet'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'door',
+            'text' => ['en' => 'Door', 'hu' => 'Ajtó'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'billing_vat_number',
+            'text' => ['en' => 'Billing Vat Number', 'hu' => 'Adószám'],
+        ]);
+        Translation::create([
+            'group' => 'order',
+            'key' => 'different_billing',
+            'text' => ['en' => 'Different Billing Address', 'hu' => 'Eltérő Számlázási Adatok'],
+        ]);
+
+    }
+
     protected function addShopRelatedTranslations() {
         Translation::create([
             'group' => 'actions',
@@ -91,11 +180,7 @@ class TranslationTableSeeder extends Seeder
             'key' => 'shop',
             'text' => ['en' => 'Shop', 'hu' => 'Üzlet'],
         ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'order',
-            'text' => ['en' => 'Order', 'hu' => 'Rendelés'],
-        ]);
+       
         Translation::create([
             'group' => 'shop',
             'key' => 'products',
@@ -105,81 +190,6 @@ class TranslationTableSeeder extends Seeder
             'group' => 'shop',
             'key' => 'categories',
             'text' => ['en' => 'Categories', 'hu' => 'Kategóriák'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'track_order_status',
-            'text' => ['en' => 'Track Order Status', 'hu' => 'Rendelés Követés'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'order_token',
-            'text' => ['en' => 'Order Token', 'hu' => 'Rendelési Azonosító'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'customer',
-            'text' => ['en' => 'Customer', 'hu' => 'Ügyfél'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'title',
-            'text' => ['en' => 'Title', 'hu' => 'Titulus'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'first_name',
-            'text' => ['en' => 'First Name', 'hu' => 'Vezetéknév'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'last_name',
-            'text' => ['en' => 'Last Name', 'hu' => 'Utónév'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'country',
-            'text' => ['en' => 'Country', 'hu' => 'Ország'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'city',
-            'text' => ['en' => 'City', 'hu' => 'Város'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'zip_code',
-            'text' => ['en' => 'Zip Code', 'hu' => 'Irányítószám'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'address',
-            'text' => ['en' => 'Address', 'hu' => 'Közterület neve'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'house_nr',
-            'text' => ['en' => 'House Number', 'hu' => 'Házszám'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'floor',
-            'text' => ['en' => 'Floor', 'hu' => 'Emelet'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'door',
-            'text' => ['en' => 'Door', 'hu' => 'Ajtó'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'billing_vat_number',
-            'text' => ['en' => 'Billing Vat Number', 'hu' => 'Adószám'],
-        ]);
-        Translation::create([
-            'group' => 'shop',
-            'key' => 'different_billing',
-            'text' => ['en' => 'Different Billing Address', 'hu' => 'Eltérő Számlázási Adatok'],
         ]);
     }
 }
