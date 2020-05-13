a5eedcaf59347e49a9dbadbb4713172afa520a90
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-13 09:30:07 +0200

    Expand Order Factory with statuses

--- src/Database/Factories/OrderFactory.php
+++ src/Database/Factories/OrderFactory.php
@@ -8,7 +8,17 @@ use Faker\Generator as Faker;
 use MayIFit\Extension\Shop\Models\Order;
 
 $factory->define(Order::class, function (Faker $faker) {
+    $statuses = [
+        'placed',
+        'in_review',
+        'approved',
+        'transferred_for_shipping',
+        'in_delivery',
+        'delivered'
+    ];
+
     return [
-        'order_token' =>  Str::random(40)
+        'order_token' =>  Str::random(40),
+        'order_status' => $statuses[$faker->numberBetween(0, count($statuses))]
     ];
 });
