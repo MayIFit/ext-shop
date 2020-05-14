ae4baca2a59c3810d8000cd420b66443cbbc2547
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-14 22:27:13 +0200

    Fixes

--- src/Models/Pivots/OrderProductPivot.php
+++ src/Models/Pivots/OrderProductPivot.php
@@ -22,8 +22,8 @@ class OrderProductPivot extends Pivot
             }
             $parent = $model->pivotParent;
             
-            $parent->net_value += $product->net_price * $model->quantity;
-            $parent->value += ($product->net_price * (1 + ($product->vat / 100))) * $model->quantity;
+            $parent->net_value += round($product->net_price * $model->quantity);
+            $parent->value += round(($product->net_price * (1 + ($product->vat / 100))) * $model->quantity);
             $parent->order_quantity += $model->quantity;
             $parent->save();
             return $model;
