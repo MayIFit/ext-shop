7711200a4188fa763aaf667c6e0e29d122d11796
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-21 13:37:04 +0200

    Added product discount to the order value calculation

--- src/Models/Pivots/OrderProductPivot.php
+++ src/Models/Pivots/OrderProductPivot.php
@@ -2,6 +2,8 @@
 
 namespace MayIFit\Extension\Shop\Models\Pivots;
 
+use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,8 +25,19 @@ class OrderProductPivot extends Pivot
             
             $order = $model->pivotParent;
             $productPricingForCurrency = $product->pricings()->where('currency', $order->currency)->first();
-            $order->net_value += $productPricingForCurrency->base_price * $model->quantity;
-            $order->gross_value += $productPricingForCurrency->gross_price * $model->quantity;
+            $now = Carbon::now();
+            $productDiscount = $product->discounts()
+                ->where(function ($query) use ($now) {
+                    $query->where('available_from', '<=', $now);
+                    $query->where('available_to', '>=', $now);
+                })
+                ->orWhere(function ($query) use ($now) {
+                    $query->where('available_from', '<=', $now);
+                    $query->whereNull('available_to');
+                })
+                ->first();
+            $order->net_value += ($productPricingForCurrency->base_price * (1 - ($productDiscount->discount_percentage / 100))) * $model->quantity;
+            $order->gross_value += ($productPricingForCurrency->gross_price * (1 - ($productDiscount->discount_percentage / 100))) * $model->quantity;
             $order->quantity += $model->quantity;
 
             $order->save();
