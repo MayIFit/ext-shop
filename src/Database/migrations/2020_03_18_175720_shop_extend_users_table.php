2097cdcb13fb86522960b01688df006d238955c4
Tischler Kristóf <tischler.kristof@gmail.com>
2020-06-08 12:06:24 +0200

    Fixes

--- src/Database/migrations/2020_03_18_175720_shop_extend_users_table.php
+++ src/Database/migrations/2020_03_18_175720_shop_extend_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class PermissionExtendUsersTable extends Migration
+class ShopExtendUsersTable extends Migration
 {
     /**
      * Run the migrations.
