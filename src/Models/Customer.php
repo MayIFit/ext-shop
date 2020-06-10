bf158a74fc4e01c7dbd82710ae51c591d1142695
Tischler Kristóf <tischler.kristof@gmail.com>
2020-06-11 01:06:59 +0200

    Fixes

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -40,6 +40,7 @@ class Customer extends Model
 
     public static function boot()
     {
+        parent::boot();
         self::creating(function($model) {
             if (!$model->different_billing) {
                 $model->billing_first_name = $model->first_name;
