142d5fb384645422d7ac09b4346679952609ba32
Tischler Kristóf <tischler.kristof@gmail.com>
2020-06-15 21:22:08 +0200

    Fix namespace

--- src/GraphQL/Queries/AllRetailUser.php
+++ src/GraphQL/Queries/AllRetailUser.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\GraphQL\Queries\Core;
+namespace App\GraphQL\Queries\Extemsions;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
