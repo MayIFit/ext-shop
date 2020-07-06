4da7c048bb4c0bd203e83bd9e0cb2f13a71ee9f6
Tischler Kristóf <tischler.kristof@gmail.com>
2020-07-06 10:22:04 +0200

    Fix classname

--- src/GraphQL/Queries/AllPaymentType.php
+++ src/GraphQL/Queries/AllPaymentType.php
@@ -6,7 +6,7 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use Illuminate\Support\Facades\DB;
 
-class AllCurrency
+class AllPaymentType
 {
     public function __invoke($rootValue,array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
         $currencies = DB::table('payment_types')->get();
