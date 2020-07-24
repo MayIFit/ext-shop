8a5f4ede76c867fabf015042d6be64fb90c1aa87
Tischler Kristóf <tischler.kristof@gmail.com>
2020-07-24 22:05:37 +0200

    Barcodes
    - added UPC and EAN code columns to the products table

--- src/Database/migrations/2019_12_12_130127_create_products_table.php
+++ src/Database/migrations/2019_12_12_130127_create_products_table.php
@@ -16,9 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('catalog_id')->unique();
+            $table->string('ean_code')->nullable();
+            $table->string('upc_code')->nullable();
             $table->string('name');
-            $table->foreignId('category_id')->nullable()->references('id')->on('product_categories');
-            $table->string('parent_id')->nullable()->references('id')->on('products');
             $table->longText('description')->nullable();
             $table->string('technical_specs')->nullable();
             $table->string('supplied')->nullable();
@@ -28,6 +28,9 @@ class CreateProductsTable extends Migration
             $table->string('out_of_stock_text')->nullable();
             $table->string('quantity_unit_text')->nullable();
             $table->boolean('refurbished');
+
+            $table->foreignId('category_id')->nullable()->references('id')->on('product_categories');
+            $table->string('parent_id')->nullable()->references('id')->on('products');
             $table->foreignId('created_by')->nullable()->references('id')->on('users');
             $table->foreignId('updated_by')->nullable()->references('id')->on('users');
             $table->timestamps();
