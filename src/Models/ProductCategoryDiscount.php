ee9650812afdcc0c74cc3de3242bf0217588c457
Tischler Kristóf <tischler.kristof@gmail.com>
2020-07-27 10:32:13 +0200

    Fix imports and relations

--- src/Models/ProductCategoryDiscount.php
+++ src/Models/ProductCategoryDiscount.php
@@ -4,14 +4,16 @@ namespace MayIFit\Extension\Shop\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 use MayIFit\Core\Permission\Traits\HasUsers;
 use MayIFit\Extension\Shop\Traits\HasReseller;
+use MayIFit\Extension\Shop\Models\ProductCategory;
 
 class ProductCategoryDiscount extends Model
 {
-    use SoftDeletes, HasUsers, HasProduct, HasReseller;
+    use SoftDeletes, HasUsers, HasReseller;
 
     /**
      * The attributes that should be cast.
@@ -34,4 +36,8 @@ class ProductCategoryDiscount extends Model
     protected $attributes = [
         'discount_percentage' => 0.00,
     ];
+
+    public function categories(): HasMany {
+        return $this->hasMany(ProductCategory::class);
+    }
 }
