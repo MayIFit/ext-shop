1238749f5a874b47a29aac1bbe4ad97aa7b8ee8f
Tischler Kristóf <tischler.kristof@gmail.com>
2020-09-15 15:05:24 +0200

    Added shopping cart manipulation testcases
    - added price checking to order placing test

---
+++ tests/Feature/CanPlaceItemsInShoppingCartTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace MayIFit\Extension\Shop\Tests\Feature;
+
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use MayIFit\Extension\Shop\Tests\TestCase;
+
+use App\Models\User;
+use MayIFit\Extension\Shop\Models\Reseller;
+use MayIFit\Extension\Shop\Models\Product;
+use MayIFit\Extension\Shop\Models\ProductPricing;
+
+class CanPlaceItemsInShoppingCartTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_can_place_new_item_in_cart(): void
+    {
+        parent::setUp();
+
+        $user = new User;
+        $user->id = 1;
+        $user->name = 'Test';
+        $user->email = 'test@test.com';
+        $user->password = 'test';
+        $user->save();
+
+        $product = factory(Product::class)->create();
+        factory(ProductPricing::class)->create([
+            'product_id' => $product->id
+        ]);
+
+        $reseller = factory(Reseller::class)->create([
+            'user_id' => $user->id
+        ]);
+
+        Sanctum::actingAs($user, ['*']);
+
+        $this->graphQL('
+            mutation {
+                createResellerShopCart(input: {
+                    product: {
+                        connect: ' . $product->id . '
+                    }
+                    quantity: 10,
+                    reseller: {
+                        connect: ' . $reseller->id . '
+                    }
+                }) {
+                    product {
+                        id
+                    }
+                }
+            }
+        ')->assertJSON([
+            'data' => [
+                'createResellerShopCart' => [
+                    'product' => [
+                        'id' => $product->id
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
